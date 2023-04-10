1c09f6bb17e411a506c9072f532c169db6e8aefa
INC02128 <amitkumar.sahu@incture.com>
2023-04-10 14:50:20 +0530

    First Commit

---
+++ API calling and collectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  API calling and collectionView
+//
+//  Created by Amit Kumar Sahu on 10/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
